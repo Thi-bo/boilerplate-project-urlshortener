53e05b4135041789ec7d46d35be403e15143d3af
thi-bo <thibauthoundjo23@email.com>
2024-06-17 21:33:21 +0100

    URL Shortener Microservice Short URL Creation

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const dns = require('dns');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const shortUrls = {};
+let shortUrlId = 0;
 const app = express();
 
 // Basic Configuration
@@ -18,6 +22,37 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// API pour raccourcir les URL
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+
+  // Vérifier si l'URL est valide
+  dns.lookup(new URL(originalUrl).hostname, (err) => {
+    if (err) {
+      res.json({ error: 'invalid url' });
+    } else {
+      const shortUrl = ++shortUrlId;
+      shortUrls[shortUrl] = originalUrl;
+      res.json({ original_url: originalUrl, short_url: shortUrl });
+    }
+  });
+});
+
+// Redirection vers l'URL d'origine
+app.get('/api/shorturl/:shortUrl', (req, res) => {
+  const shortUrl = req.params.shortUrl;
+  const originalUrl = shortUrls[shortUrl];
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.json({ error: 'invalid short url' });
+  }
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
